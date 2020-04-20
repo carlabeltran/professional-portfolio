b26b11596af38512780f121e8201cb7350802f23
carlabeltran <carlabeltran14@icloud.com>
2020-04-20 18:28:14 -0500

    Completed menu overlay and all links

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -27,5 +27,38 @@ $(document).ready(function () {
       $(this).css("background-color", "#40e0d0");
     },
   });
+  $(".transbox-work").on({
+    mouseenter: function () {
+      $(this).css("background-color", "#b19cd9");
+    },
+    mouseleave: function () {
+      $(this).css("background-color", "transparent");
+    },
+    click: function () {
+      $(this).css("background-color", "#b19cd9");
+    },
+  });
+  $(".transbox-contact").on({
+    mouseenter: function () {
+      $(this).css("background-color", "#ffff00");
+    },
+    mouseleave: function () {
+      $(this).css("background-color", "transparent");
+    },
+    click: function () {
+      $(this).css("background-color", "#ffff00");
+    },
+  });
+  $(".transbox-blog").on({
+    mouseenter: function () {
+      $(this).css("background-color", "#00ff00");
+    },
+    mouseleave: function () {
+      $(this).css("background-color", "transparent");
+    },
+    click: function () {
+      $(this).css("background-color", "#ffff00");
+    },
+  });
 });
 
