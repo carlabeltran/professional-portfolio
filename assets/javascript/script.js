d9d5484ab1ba00ff99059ddc2b947275fbc3323a
carlabeltran <carlabeltran14@icloud.com>
2020-04-20 16:00:37 -0500

    Menu overlay in the works fully functional open and close buttons completed the first link HOME

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+console.log("JAVA && HTML ARE LINKED!!")
+
+// OPEN MENU
+function openNav() {
+  document.getElementById("menu").style.width = "100%";
+}
+openNav();
+
+
+// CLOSE MENU
+function closeNav() {
+  document.getElementById("menu").style.width = "0%";
+}
+closeNav();
