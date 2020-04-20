7a3a1737faca225867aeb2fed7d344e22ad91208
carlabeltran <carlabeltran14@icloud.com>
2020-04-20 17:43:41 -0500

    Completed About hover effect on menu overlay

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,3 +12,20 @@ function closeNav() {
   document.getElementById("menu").style.width = "0%";
 }
 closeNav();
+
+//HOVER EFFECT FOR ABOUT ON MENU OVERLAY
+$(document).ready(function () {
+  
+  $(".transbox-about").on({
+    mouseenter: function () {
+      $(this).css("background-color", "#40e0d0");
+    },
+    mouseleave: function () {
+      $(this).css("background-color", "transparent");
+    },
+    click: function () {
+      $(this).css("background-color", "#40e0d0");
+    },
+  });
+});
+
